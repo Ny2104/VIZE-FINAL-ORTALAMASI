6e61ce9da6fa71625c9b29ac967f8068fe60a8e6
NURŞAH YILDIZ <103890418+Ny2104@users.noreply.github.com>
2024-03-07 14:07:59 +0300

    VIZE FINAL ORTALAMASI
    Kullanıcıdan alınan vize ve final notlarına dayalı olarak başarı durumunu değerlendirir ve sonucu kullanıcıya bildirir.

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+let vize1 = Number(prompt("Vize 1 notunuzu giriniz :"));
+let vize2 = Number(prompt("Vize 2 notunuzu giriniz :"));
+
+let final= Number(prompt("Final notunuzu giriniz :"));
+
+
+let ortalama = (vize1*0.3) + (vize2*0.3) + (final*0.4);
+
+if(ortalama>=60){
+    alert("Dersten geçtiniz tebrikler :)) -->" + ortalama);
+    console.log("Dersten geçtiniz tebrikler :))");
+}else{
+    alert("Kaldınız , geçmiş olsun :( -->" + ortalama);
+    console.log("Kaldınız , geçmiş olsun :(");
+
+}
